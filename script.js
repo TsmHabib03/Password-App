d9e23bc57061b0e314f65b1df1b5f33f7ba02046
TsmHabib03 <jaudianhabib879@gmail.com>
2025-01-08 16:40:22 +0800

    update script.js

--- script.js
+++ script.js
@@ -2,14 +2,20 @@
 document.getElementById('displayButton').addEventListener('click', function() {
   const passwordInput = document.getElementById('password').value;
   const usernameInput = document.getElementById('username').value;
+  const usernameInput1=usernameInput.includes("@");
   const passwordDisplay = document.getElementById('passwordDisplay');
   const usernameDisplay=document.getElementById('usernameDisplay');
 
-  if (passwordInput, usernameInput) {
-    usernameDisplay.textContent= `Your username is: ${usernameInput}`;
+  if (passwordInput) {
+    
     passwordDisplay.textContent = `Your password is: ${passwordInput}`;
   } else {
-    usernameDisplay.textContent=`Please enter a username`;
     passwordDisplay.textContent = 'Please enter a password!';
+  }
+  if(usernameInput1){
+    usernameDisplay.textContent= `Your username is: ${usernameInput}`;
+  }
+  else {
+  usernameDisplay.textContent = `Please enter a username`;
   }
 });
