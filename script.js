97fe20ac6f5abe8e111ce53d50e372c1355177d5
TsmHabib03 <jaudianhabib879@gmail.com>
2025-01-08 16:10:34 +0800

    update

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // script.js
 document.getElementById('displayButton').addEventListener('click', function() {
   const passwordInput = document.getElementById('password').value;
+  const usernameInput = document.getElementById('username').value;
   const passwordDisplay = document.getElementById('passwordDisplay');
+  const usernameDisplay=document.getElementById('usernameDisplay');
 
-  if (passwordInput) {
+  if (passwordInput, usernameInput) {
+    usernameDisplay.textContent= `Your username is: ${usernameInput}`;
     passwordDisplay.textContent = `Your password is: ${passwordInput}`;
   } else {
+    usernameDisplay.textContent=`Please enter a username`;
     passwordDisplay.textContent = 'Please enter a password!';
   }
 });
